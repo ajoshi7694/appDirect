c4b82e843c57eba2ed90564cbbcccc18d9f14ee4
Ashish Joshi <ajoshi73@uwo.ca>
2020-11-17 16:32:04 -0500

    Level 2

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // String without a space or any specail char
+//| II     | hello world            | ellohay orldway              |     
 
-//hhello                  | ellohhay        
 
-
-//Assuming String without a space or any specail char
+//Assuming String with a space
 function getPigLatin(input){
     let r ="";
     let start = "";
-    var arr= input.split("");
     for(let i=0; i<input.length;i++){
         if(input[i] === 'a' || input[i]=== 'e' || input[i] === 'i' || input[i]==='o' || input[i]==='u'){
             start = input.slice(0,i)
@@ -18,5 +16,14 @@ function getPigLatin(input){
     return r;
 }
 
-let res= getPigLatin("hello");
+function getResult(inp){
+    let result= ""
+    var arr= inp.split(" ");
+    arr.forEach(elm=>{
+        result = result + getPigLatin(elm) + " ";
+    });
+    return result.trim();
+}
+
+let res= getResult("hello world");
 console.log(res);
